5486ec5d717373bc4f3688723628054468b14dfd
aleedca <acerdas1701@estudiantec.cr>
2024-01-30 17:28:49 -0600

    changes section projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import '../styles/Projects.css';
-import imgGithub from '../assets/images/githubdark.png';
-import imgLink from '../assets/images/linkdark.png';
-import imgPlant from '../assets/images/plant.png';
+import imgGithub from '../assets/images/githublight.png';
+import imgLink from '../assets/images/linklight.png';
 
 const Card = (props) => {
     return (
         <div className='card'>
             <div className='card-content'>
-                <div className='card-logo'>
-                    <img src={imgPlant} alt="" />
-                </div>
-
                 <div className='card-header'>
-                    <h3>{props.title}</h3>
+                    {props.title} 
+                    <span>{props.type}</span>
                 </div>
 
                 <div className='card-body'>
-                    <h3>{props.type}</h3>
-                    <p>Descripcion aqui</p>
+                    <p>{props.description}</p>
                 </div>
 
-                <div className='card-footer'>
+                <div className='card-technologies'>
+                    {props.tech.map((tech) => {
+                        return <span>{tech} </span>
+                    })}
+                </div>
+
+                <div className='card-links'>
                     <a href={props.github} target='_blank' rel="noreferrer"><img src={imgGithub} alt="" /></a>
                     <a href={props.website} target='_blank' rel="noreferrer"><img src={imgLink} alt="" /></a>
                 </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,19 +2,54 @@ import React from 'react';
 import '../styles/Projects.css';
 import Card from '../components/Card.js';
 
+let projects = [
+    {
+        title: 'Tic Tac Toe',
+        type: 'Proyecto personal',
+        tech: ['React', 'HTML', 'CSS', 'JS'],
+        description: 'Simple videojuego web, puede jugarse con dos jugadores en un mismo dispositivo. El modo de juego para un jugador estará haciéndose próximamente. ',
+        website: 'https://aleedca.github.io/TicTacToe/',
+        github: 'https://github.com/aleedca/TicTacToe'
+    },
+
+    {
+        title: 'HeyAso',
+        type: 'Trabajo práctico',
+        tech: ['HTML', 'CSS', 'JS', 'Firebase'],
+        description: 'Sistema para las asociaciones del TEC Costa Rica, brinda la facilidad de administrar eventos y actividades en ellas para fomentar la participación de los estudiantes en período lectivo.',
+        website: 'https://marilau-re.github.io/HeyAso/',
+        github: 'https://github.com/MariLau-RE/HeyAso'
+    },
+
+    {
+        title: 'UpsideDown',
+        type: 'Hackathon - WGJ 2022',
+        tech: ['Unity', 'C#'],
+        description: 'Videojuego hecho en 48 horas de forma grupal, este le permite al jugador aprender de los diferentes niveles a punta de prueba y error con el fin de llegar a la victoria y rescatar lo perdido.',
+        website: 'https://fiozelaya.itch.io/upside-down',
+        github: 'https://github.com/aleedca/WomenGameJam22'
+    },
+
+    {
+        title: 'Biblio-Booking',
+        type: 'Trabajo práctico',
+        tech: ['HTML', 'CSS', 'JS', 'Firebase'],
+        description: 'Sistema para alquilar cubículos de estudio en una biblioteca de forma rápida y sencilla, de manera que el proceso se evite ir presencialmente a la bilbioteca para tramitar el préstamo de los cubículos.',
+        website: 'https://erick13as.github.io/ProyectoReque/mainPage.html',
+        github: 'https://github.com/Erick13as/ProyectoReque'
+    }
+]
+
 const Projects = () => {
     return (
         <div id='projects' className='projects'>
             <div className='title-section'> <h2>{"< projects />"}</h2> </div>
 
             <div className='container-cards'>
-                <Card title='Tic Tac Toe' type='Proyecto personal' website='https://aleedca.github.io/TicTacToe/' github='https://github.com/aleedca/TicTacToe'/>
-                <Card title='HeyAso' type='Proyecto universitario' website='https://marilau-re.github.io/HeyAso/' github='https://github.com/MariLau-RE/HeyAso'/>
-                <Card title='UpsideDown' type='Hackathon - WGJ' website='https://fiozelaya.itch.io/upside-down' github='https://github.com/aleedca/WomenGameJam22' photo=''/>
-                <Card title='Shades of Nüwa' type='Hackathon - GJ+' website='https://fiozelaya.itch.io/shades-of-nuwa' github='https://github.com/aleedca/GameJamPlus2022' photo=''/>
-                <Card title='????' type='Proyecto personal' website='' github='' photo=''/>
-                <Card title='Biblio-Booking' type='Proyecto universitario' website='https://erick13as.github.io/ProyectoReque/mainPage.html' github='https://github.com/Erick13as/ProyectoReque' photo=''/>
-            </div>
+                {projects.map((project) => {
+                    return <Card title={project.title} type={project.type} tech={project.tech} description={project.description} website={project.website} github={project.github} />    
+                })}
+                </div>
         </div>
     );
 }
